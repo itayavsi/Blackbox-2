bd37e51559f448cac47d3c43b14b953f9b6cb85e
itayavsi <147296749+itayavsi@users.noreply.github.com>
2025-03-26 16:17:18 +0200

    Update client2.py

--- client2.py
+++ client2.py
@@ -28,40 +28,24 @@ def solve_level_1():
     response = requests.get(f"{BASE_URL}/get-level1")
     if response.status_code == 200:
         challenge_info = response.json()
-        base_directory = challenge_info.get("base_directory")
-        env_var = challenge_info.get("environment_variable")
         
-        print("Challenge:", challenge_info.get("challenge"))
-        print("Base Directory:", base_directory)
-        print(f"Current {env_var} status:", os.environ.get(env_var, 'Not set'))
-        
-        # Search for flag files
-        for root, dirs, files in os.walk(base_directory):
-            for file in files:
-                if file == "system_log.txt":
-                    file_path = os.path.join(root, file)
+    print("\nPossible steps to solve:")
+    print("1. Change the environment variable LockAdministrator to '0'")
+    print("2. Use 'import os' and 'os.environ[\"LockAdministrator\"] = \"0\"'")
+    print("Press Enter after Unlock the Administrator to Refreash and revel the flag")
                     
-                    print("\nPossible steps to solve:")
-                    print("1. Change the environment variable LockAdministrator to '0'")
-                    print("2. Use 'import os' and 'os.environ[\"LockAdministrator\"] = \"0\"'")
-                    
-                    ans = input("\nEnter the flag from the system_log.txt: ").strip()
+    ans = input("\nEnter the flag from the system_log.txt: ").strip()
                     
                     # Attempt to submit the flag
-                    post_resp = requests.post(f"{BASE_URL}/solve-level1", json={"answer": ans})
-                    if post_resp.status_code == 200:
-                        print(post_resp.json().get("message"))
-                        print("Level 1 completed.")
-                        return True
-                    else:
-                        print(post_resp.json().get("message"))
-                        return solve_level_1()
-        
-        print("No flag files found.")
-        return False
+    post_resp = requests.post(f"{BASE_URL}/solve-level1", json={"answer": ans})
+    if post_resp.status_code == 200:
+      print(post_resp.json().get("message"))
+      print("Level 1 completed.")
+      return True
     else:
-        print("Failed to get Level 1 challenge.")
-        return False
+       print(post_resp.json().get("message"))
+       return solve_level_1()
+
 
 def main():
     print("Starting CTF Challenge...")
