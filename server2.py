8a5682928b6323bba641e0b57db4678c9dfc69cb
itayavsi <147296749+itayavsi@users.noreply.github.com>
2025-03-26 11:53:18 +0200

    Update server2.py

--- server2.py
+++ server2.py
@@ -7,18 +7,18 @@ from flask import jsonify, make_response, request
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32))
 
-FLAG_1 = "42"     
+FLAG_0 = "42"     
 
-@app.route('/get-level1', methods=["GET"])
-def get_level1():
+@app.route('/get-level0', methods=["GET"])
+def get_level0():
     return jsonify({"binary": "101010"})
 
-@app.route('/solve-level1', methods=["POST"])
+@app.route('/solve-level0', methods=["POST"])
 def solve_level1():
     data = request.get_json()
     answer = data.get("answer")
-    if answer == FLAG_1:
-        return make_response(jsonify({"message": "Level 1 solved!"}), 200)
+    if answer == FLAG_0:
+        return make_response(jsonify({"message": "Level 0 solved!"}), 200)
     return make_response(jsonify({"message": "Incorrect answer. Try again."}), 400)
 
 if __name__ == "__main__":
